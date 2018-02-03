ddaa172e58543c177ed540487c5fc56bb0e8afc8
roy reznik <royreznik@gmail.com>
2018-02-03 19:04:38 +0200

    Marzuk version + minor changes

---
+++ Etgarim 4.1.0/DefenseHandler.java
@@ -0,0 +1,13 @@
+package bots;
+
+import java.util.*;
+
+public class DefenseHandler
+{
+    public List<BotPirate> defenders;
+    
+    public DefenseHandler()
+    {
+        defenders = new ArrayList<>();
+    }
+}
